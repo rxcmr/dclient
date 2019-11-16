b31dae9c0c1f6c8741432a4eb695e806d56a1e75
rxcmr <lythe1107@icloud.com>
2019-11-16 16:25:31 +0800

    mega license 4.0
    Signed-off-by: rxcmr <lythe1107@icloud.com>

--- src/main/java/dcl/Flesh.java
+++ src/main/java/dcl/Flesh.java
@@ -1,7 +1,7 @@
 package dcl;
 
 /*
- * Copyright 2019 rxcmr <lythe1107@gmail.com>.
+ * Copyright 2019 rxcmr <lythe1107@gmail.com> or <lythe1107@icloud.com>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,6 +14,22 @@ package dcl;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * dclient, a JDA Discord bot
+ *      Copyright (C) 2019 rxcmr <lythe1107@gmail.com> or <lythe1107@icloud.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 import com.jagrosh.jdautilities.command.Command;
@@ -25,7 +41,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author rxcmr
+ * @author rxcmr <lythe1107@gmail.com> or <lythe1107@icloud.com>
  */
 public class Flesh {
   public Flesh() throws ReflectiveOperationException {
--- src/main/java/dcl/commands/TestCommand.java
+++ src/main/java/dcl/commands/TestCommand.java
@@ -1,7 +1,7 @@
 package dcl.commands;
 
 /*
- * Copyright 2019 rxcmr <lythe1107@gmail.com>.
+ * Copyright 2019 rxcmr <lythe1107@gmail.com> or <lythe1107@icloud.com>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,6 +14,22 @@ package dcl.commands;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * dclient, a JDA Discord bot
+ *      Copyright (C) 2019 rxcmr <lythe1107@gmail.com> or <lythe1107@icloud.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 import com.jagrosh.jdautilities.command.Command;
@@ -27,7 +43,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * @author rxcmr
+ * @author rxcmr <lythe1107@gmail.com> or <lythe1107@icloud.com>
  */
 @SuppressWarnings("unused")
 public class TestCommand extends Command {
